477c229938e8198ba85284fa18eba0c6b14f6adb
muslummemmedzadee@gmail.com <muslummemmedzadee@gmail.com>
2024-06-05 19:01:44 +0400

    magic starts ready

--- src/Components/MainComponent/Add.jsx
+++ src/Components/MainComponent/Add.jsx
@@ -1,21 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../Style/Pages/Add.css'
 import  {Button}  from "@mui/material";
-import { styled } from '@mui/material/styles';
+import { IoIosArrowForward } from "react-icons/io";
+import Starts from "../Modals/Starts";
 
 
 function Add() {
-  const VisuallyHiddenInput = styled('input')({
-    clip: 'rect(0 0 0 0)',
-    clipPath: 'inset(50%)',
-    height: 1,
-    overflow: 'hidden',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    whiteSpace: 'nowrap',
-    width: 1,
-  });
+  const [state, setState] = useState(false);
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
   return (
     <>
       <div className="video-boxes">
@@ -29,9 +27,9 @@ function Add() {
       role={undefined}
       variant="contained"
       tabIndex={-1}
+      onClick={toggleDrawer("right",true)}
     >
       <p className="inp-text">+</p>
-      <VisuallyHiddenInput type="file" className="inp-add" />
     </Button>
           </div>
         </div>
@@ -52,11 +50,11 @@ function Add() {
       tabIndex={-1}
     >
   <p className="inp-text">+</p>
-      <VisuallyHiddenInput type="file" className="inp-add" />
     </Button>
           </div>
         </div>
       </div>
+      <Starts state={state} toggleDrawer={toggleDrawer} />
     </>
   );
 }
