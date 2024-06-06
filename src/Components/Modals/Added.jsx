692e3ca532c8a6bdd5ce74b829d9ac644faf97c9
muslummemmedzadee@gmail.com <muslummemmedzadee@gmail.com>
2024-06-06 18:22:19 +0400

    added duzledildi

--- src/Components/Modals/Added.jsx
+++ src/Components/Modals/Added.jsx
@@ -1,19 +1,80 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import "../../Style/Pages/ModalStyle/Added.css";
+import { FaArrowLeft } from "react-icons/fa6";
+import Button from "@mui/material/Button";
 
-function Added({savedVideos}) {
-    const videoRef = useRef(null);
+function Added({ savedVideos }) {
+  const videoRef = useRef(null);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    const handleTimeUpdate = () => {
+      setCurrentTime(video.currentTime);
+    };
+
+    const handleLoadedMetadata = () => {
+      setDuration(video.duration);
+    };
+
+    video.addEventListener("timeupdate", handleTimeUpdate);
+    video.addEventListener("loadedmetadata", handleLoadedMetadata);
+
+    return () => {
+      video.removeEventListener("timeupdate", handleTimeUpdate);
+      video.removeEventListener("loadedmetadata", handleLoadedMetadata);
+    };
+  }, []);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
+
   return (
     <>
-      <div className="add-video">
-        <video controls={false}  ref={videoRef} autoPlay>
-            <source src={savedVideos} type="video/mp4" />
+      <div className="added-video">
+        <video controls={false} ref={videoRef} autoPlay>
+          <source src={savedVideos} type="video/mp4" />
         </video>
         <div className="custom-controls">
-            <input type="range" min="0" max="100"   />
+          <input
+            className="current-inp"
+            type="range"
+            min="0"
+            max={duration}
+            value={currentTime}
+            readOnly
+          />
+          <div className="controls-top">
+            <FaArrowLeft className="added-arrow" />
+            <div className="controls-right">
+                    <div className="added-time">
+              <span className="time">
+                {formatTime(currentTime)}/{formatTime(duration)}
+              </span>
+            </div>
+            <p className="x2">2X</p>
+            </div>
+        
+          </div>
+          <div className="controls-bottom">
+            <Button
+              component="label"
+              role={undefined}
+              variant="contained"
+              tabIndex={-1}
+              startIcon=""
+            >
+              Get a consultation!
+            </Button>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Added
+export default Added;
